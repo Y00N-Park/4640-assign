c121dd3c5cc288b6ff3457db2a8bee3700469814
SJ Park <55506518+Y00N-Park@users.noreply.github.com>
2022-11-24 19:02:41 -0800

    Update main.tf

--- dev/main.tf
+++ dev/main.tf
@@ -12,14 +12,6 @@ provider "digitalocean" {
   token = var.do_token
 }
 
-#data "digitalocean_ssh_key" "ssh_key" {
-#  name = "ACIT4640"
-#}
-
-#data "digitalocean_project" "lab_project" {
-#  name = "first-project"
-#}
-
 # Create a new tag
 resource "digitalocean_tag" "do_tag" {
   name = "assignment"
